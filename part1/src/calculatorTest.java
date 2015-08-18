e0e80aa765e20e7551252ce9baa02fa00009d188
billbillbillbill <billo@qq.com>
2015-08-18 16:05:38 +0800

    add junit files

---
+++ part1/src/calculatorTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class calculatorTest {
+	public calculator calculator = new calculator();
+	@Test
+	public void testCalculator() {
+		assertEquals("1", "1");
+	}
+}
